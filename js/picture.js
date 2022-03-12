f6abdfbeaaa275d9b7b4495ba6bd301fcced3cf6
Irina Sitnikova <irina_sit88@mail.ru>
2022-03-12 13:04:47 +0300

    Отрисуй меня полностью, исправление ошибок

--- js/picture.js
+++ js/picture.js
@@ -1,19 +1,19 @@
 import {createDescription} from './data.js';
 
-const picturesListElement = document.querySelector('.big-picture__social');
+const picturesListElement = document.querySelector('.pictures.container');
 const patternPicture = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const randomUserImages = createDescription();
+const randomUserImages = createDescription(11);
 
 const picturesListFragment = document.createDocumentFragment();
 
 randomUserImages.forEach(({url, comments, likes}) => {
   const pictureElement = patternPicture.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__comments').textContent = comments;
-  pictureElement.querySelector('.picture__likes').number = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
   picturesListFragment.appendChild(pictureElement);
 });
 
