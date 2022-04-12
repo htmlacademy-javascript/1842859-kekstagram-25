4cdefc0fcac01eb74de6c07ee85d1748bb9cc36c
Irina Sitnikova <irina_sit88@mail.ru>
2022-04-12 09:29:19 +0300

    Правда или действие (часть 2)

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,11 +1,11 @@
 import {isEscapeKey} from './util.js';
 
 const bigPicture = document.querySelector('.big-picture');
-const socialCommentCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
 const body = document.querySelector('body');
 const commentsList = bigPicture.querySelector('.social__comments');
 const commentTemplate = commentsList.querySelector('li');
+const LIMIT_COMMENTS = 5;
 
 const onBigPictureEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -22,12 +22,12 @@ function hideBigPicture () {
 }
 
 const showBigPicture = ({url, description, comments, likes}) => {
-  socialCommentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
+  commentsLoader.classList.remove('hidden');
 
   bigPicture.querySelector('.big-picture__img img').src = url;
   bigPicture.querySelector('.social__caption').line = description;
   bigPicture.querySelector('.likes-count').textContent = likes;
+  bigPicture.querySelector('.social__comment-count').textContent = `${comments.length > 5 ? LIMIT_COMMENTS : comments.length} из ${comments.length} комментариев`;
 
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
@@ -37,14 +37,26 @@ const showBigPicture = ({url, description, comments, likes}) => {
   cancelButton.addEventListener('click', hideBigPicture);
 
   commentsList.innerHTML = '';
-  comments.forEach((element) => {
-    const commentElement = commentTemplate.cloneNode(true);
-    commentElement.querySelector('.social__picture').src = element.avatar;
-    commentElement.querySelector('.social__picture').alt = element.name;
-    commentElement.querySelector('.social__text').textContent = element.message;
 
-    commentsList.appendChild(commentElement);
-  });
+  drawsСomments(comments.slice(0, LIMIT_COMMENTS));
+
+  function drawsСomments (arrayComments) {
+    arrayComments.forEach((element) => {
+      const commentElement = commentTemplate.cloneNode(true);
+      commentElement.querySelector('.social__picture').src = element.avatar;
+      commentElement.querySelector('.social__picture').alt = element.name;
+      commentElement.querySelector('.social__text').textContent = element.message;
+
+      commentsList.appendChild(commentElement);
+    });
+  }
+
+  const uploadsСomments = () => {
+    drawsСomments(comments.slice(LIMIT_COMMENTS, 10));
+  };
+
+  commentsLoader.addEventListener('click', uploadsСomments);
+
 };
 
 export {showBigPicture, body};
