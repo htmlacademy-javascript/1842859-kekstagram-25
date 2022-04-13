7c2321b2fa7cb86b53f07993cbc35bbe151b77e7
Irina Sitnikova <irina_sit88@mail.ru>
2022-04-13 20:28:09 +0300

    Правда или действие (часть 2), исправляет ошибки

--- js/big-picture.js
+++ js/big-picture.js
@@ -5,29 +5,23 @@ const commentsLoader = document.querySelector('.comments-loader');
 const body = document.querySelector('body');
 const commentsList = bigPicture.querySelector('.social__comments');
 const commentTemplate = commentsList.querySelector('li');
-const LIMIT_COMMENTS = 5;
-
-const onBigPictureEscKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    hideBigPicture();
-  }
-};
-
-function hideBigPicture () {
-  bigPicture.classList.add('hidden');
-  body.classList.remove('modal-open');
-
-  document.removeEventListener('keydown', onBigPictureEscKeydown);
-}
+const socialCommentCount = document.querySelector('.social__comment-count');
+let limitComments = 5;
 
 const showBigPicture = ({url, description, comments, likes}) => {
   commentsLoader.classList.remove('hidden');
 
+  const onBigPictureEscKeydown = (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      hideBigPicture();
+    }
+  };
+
   bigPicture.querySelector('.big-picture__img img').src = url;
   bigPicture.querySelector('.social__caption').line = description;
   bigPicture.querySelector('.likes-count').textContent = likes;
-  bigPicture.querySelector('.social__comment-count').textContent = `${comments.length > 5 ? LIMIT_COMMENTS : comments.length} из ${comments.length} комментариев`;
+  bigPicture.querySelector('.social__comment-count').textContent = `${comments.length > 5 ? limitComments : comments.length} из ${comments.length} комментариев`;
 
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
@@ -38,7 +32,12 @@ const showBigPicture = ({url, description, comments, likes}) => {
 
   commentsList.innerHTML = '';
 
-  drawsСomments(comments.slice(0, LIMIT_COMMENTS));
+  if (limitComments >= comments.length) {
+    limitComments = comments.length;
+    commentsLoader.classList.add('hidden');
+  }
+
+  drawsСomments(comments.slice(0, limitComments));
 
   function drawsСomments (arrayComments) {
     arrayComments.forEach((element) => {
@@ -52,8 +51,25 @@ const showBigPicture = ({url, description, comments, likes}) => {
   }
 
   const uploadsСomments = () => {
-    drawsСomments(comments.slice(LIMIT_COMMENTS, 10));
+    drawsСomments(comments.slice(limitComments, limitComments + 5));
+    limitComments = limitComments + 5;
+    if (limitComments >= comments.length) {
+      limitComments = comments.length;
+      commentsLoader.classList.add('hidden');
+    }
+    socialCommentCount.textContent = `${comments.length > 5 ? limitComments : comments.length} из ${comments.length} комментариев`;
   };
+
+  function hideBigPicture () {
+    bigPicture.classList.add('hidden');
+    body.classList.remove('modal-open');
+    limitComments = 5;
+    commentsList.innerHTML = '';
+
+
+    document.removeEventListener('keydown', onBigPictureEscKeydown);
+    commentsLoader.removeEventListener('click', uploadsСomments);
+  }
 
   commentsLoader.addEventListener('click', uploadsСomments);
 
